89b8e9aa858592dc2cf0b5a3e046f490857a28db
Valunshy <user@example.com>
2026-01-04 11:41:45 +0200

    Fix NameError in models and add stones stock migrations

--- manufacturing/models.py
+++ manufacturing/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.utils import timezone
 from django.utils.translation import gettext_lazy as _
 from inventory.models import Carat, Branch, Item, RawMaterial
 
